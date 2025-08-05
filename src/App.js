876a5032f2c343cefe37dbd4012ff2d49e86023d
João Paulo Radd <jpradd@yahoo.com.br>
2025-08-05 17:00:28 -0300

    Primeira versão do projeto
    versão funcional gerada com pequenas adaptações do curso feito pela Udemy

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react"
+import './estilo.css';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase : ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      "Uma nova oportunidade baterá à sua porta quando menos esperar.",
+      "Confie mais em sua intuição; ela conhece o caminho.",
+      "Pequenos passos hoje resultam em grandes conquistas amanhã.",
+      "O riso que você espalha volta para você multiplicado.",
+      "Um velho sonho pode renascer se você lhe der atenção.",
+      "A sorte favorece quem age com coragem e coração.",
+      "Você encontrará sabedoria em um lugar inesperado.",
+      "Desapegar-se do passado abre espaço para o novo.",
+      "Uma conexão hoje pode virar parceria valiosa amanhã.",
+      "A gentileza que você oferece é o eco do seu legado."
+      ];
+  }
+
+  quebraBiscoito(){
+    let state = this.state;
+    let atualiza = true;
+    do{
+      let nAleatorio= Math.floor(Math.random() * this.frases.length);
+      if (!this.state.textoFrase.includes(this.frases[nAleatorio])){
+        state.textoFrase = '" '+ this.frases[nAleatorio] +' "';
+        atualiza = false;
+      }
+    }while(atualiza)
+    
+    this.setState(state);
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <h1>React em Biscoito da sorte</h1>
+        <img src={require('./assets/biscoito.png')} className="img"/>
+        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    )
+  }
+}
+
+class Botao extends Component{
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
